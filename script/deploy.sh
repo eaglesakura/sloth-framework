c22af062c63f3cea3c83d6975d3812285cd21411
eaglesakura <eagle.sakura@gmail.com>
2016-03-29 11:42:43 +0900

    テスト追加とビルドスクリプト共有化

--- script/deploy.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-./gradlew dependencies > dependencies.txt
-./gradlew clean build javadoc uploadArchives uploadJavadoc
---
+++ src/test/java/com/eaglesakura/android/framework/FrameworkCentralTest.java
@@ -0,0 +1,10 @@
+package com.eaglesakura.android.framework;
+
+import org.junit.Test;
+
+public class FrameworkCentralTest extends UnitTestCase {
+    @Test
+    public void stub() {
+
+    }
+}
