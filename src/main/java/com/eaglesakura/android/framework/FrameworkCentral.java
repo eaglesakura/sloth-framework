f9090d27c5c8d0248f1ab965e1e61c3371097b71
eaglesakura <eagle.sakura@gmail.com>
2016-04-08 15:11:29 +0900

    Central設計を修正

--- src/main/java/com/eaglesakura/android/framework/FrameworkCentral.java
+++ src/main/java/com/eaglesakura/android/framework/FrameworkCentral.java
@@ -7,10 +7,17 @@ import com.eaglesakura.android.rx.RxTask;
 import com.eaglesakura.android.rx.RxTaskBuilder;
 import com.eaglesakura.android.rx.SubscribeTarget;
 import com.eaglesakura.android.rx.SubscriptionController;
+import com.eaglesakura.android.thread.ui.UIHandler;
 import com.eaglesakura.android.util.ContextUtil;
-import com.eaglesakura.util.LogUtil;
 
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Activity;
 import android.app.Application;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import java.lang.reflect.Method;
 
@@ -20,53 +27,125 @@ import rx.subjects.BehaviorSubject;
  *
  */
 public class FrameworkCentral {
-    private static Application application;
-    private static FrameworkApplication frameworkApplication;
 
-    /**
-     * 基本設定
-     */
-    private static BasicSettings settings;
+    private static class FrameworkCentralImpl implements Application.ActivityLifecycleCallbacks {
+        @NonNull
+        final Application mApplication;
 
-    private static SubscriptionController gSubscriptionController = new SubscriptionController();
+        @Nullable
+        final FrameworkApplication mFrameworkApplication;
+
+        @NonNull
+        final BasicSettings mSettings;
+
+
+        @NonNull
+        final BehaviorSubject<LifecycleState> mSubject = BehaviorSubject.create(LifecycleState.NewObject);
+
+        @NonNull
+        final SubscriptionController mSubscriptionController = new SubscriptionController();
+
+        int mForegroundActivities;
+
+        public FrameworkCentralImpl(@NonNull Application application) {
+            mApplication = application;
+            if (application instanceof FrameworkApplication) {
+                mFrameworkApplication = (FrameworkApplication) application;
+            } else {
+                mFrameworkApplication = null;
+            }
+            mSettings = new BasicSettings(mApplication);
+
+            mApplication.registerActivityLifecycleCallbacks(this);
+            loadSettings();
+
+            mSubscriptionController.bind(mSubject);
+            mSubject.onNext(LifecycleState.OnCreated);
+            mSubject.onNext(LifecycleState.OnStarted);
+        }
+
+        void loadSettings() {
+            // 設定を読み出す
+            final String oldVersionName = mSettings.getLastBootedAppVersionName();
+            final String versionName = ContextUtil.getVersionName(mApplication);
+
+            final int oldVersionCode = mSettings.getLastBootedAppVersionCode();
+            final int versionCode = ContextUtil.getVersionCode(mApplication);
+
+            FwLog.system("VersionCode [%d] -> [%d]", oldVersionCode, versionCode);
+            FwLog.system("VersionName [%s] -> [%s]", oldVersionName, versionName);
+
+            mSettings.setLastBootedAppVersionCode(versionCode);
+            mSettings.setLastBootedAppVersionName(versionName);
+            mSettings.commit();
+
+            // バージョンコードかバージョン名が変わったら通知を行う
+            if (mFrameworkApplication != null && ((versionCode != oldVersionCode) || (!oldVersionName.equals(versionName)))) {
+                mFrameworkApplication.onApplicationUpdated(oldVersionCode, versionCode, oldVersionName, versionName);
+            }
+
+        }
+
+        @Override
+        public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+        }
+
+        @Override
+        public void onActivityStarted(Activity activity) {
+        }
+
+        @Override
+        public void onActivityResumed(Activity activity) {
+            ++mForegroundActivities;
+            if (mForegroundActivities == 1) {
+                mSubject.onNext(LifecycleState.OnResumed);
+
+                // フォアグラウンドに移動した
+                if (mFrameworkApplication != null) {
+                    mFrameworkApplication.onApplicationForeground(activity);
+                }
+            }
+            FwLog.system("onActivityResumed num[%d] (%s)", mForegroundActivities, activity.toString());
+        }
+
+        @Override
+        public void onActivityPaused(Activity activity) {
+            FwLog.system("onActivityPaused num[%d] (%s)", mForegroundActivities, activity.toString());
+            UIHandler.postDelayedUI(() -> {
+                --mForegroundActivities;
+                // バックグラウンドに移動した
+                if (mForegroundActivities == 0 && mFrameworkApplication != null) {
+                    mFrameworkApplication.onApplicationBackground();
+                    mSubject.onNext(LifecycleState.OnPaused);
+                }
+            }, 100);
+        }
+
+        @Override
+        public void onActivityStopped(Activity activity) {
+        }
+
+        @Override
+        public void onActivityDestroyed(Activity activity) {
+
+        }
+
+        @Override
+        public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+        }
+    }
+
+    private static FrameworkCentralImpl sImpl;
 
     /**
      * Application#onCreateで呼び出す
      */
     public static void onApplicationCreate(Application application) {
-        FrameworkCentral.application = application;
-
-        if (application instanceof FrameworkApplication) {
-            FrameworkCentral.frameworkApplication = (FrameworkApplication) application;
+        sImpl = new FrameworkCentralImpl(application);
+        if (sImpl.mFrameworkApplication != null) {
+            sImpl.mFrameworkApplication.onRequestSaveCentral(sImpl);
         }
-
-        settings = new BasicSettings(application);
-        // 設定を読み出す
-        {
-            final String oldVersionName = settings.getLastBootedAppVersionName();
-            final String versionName = ContextUtil.getVersionName(application);
-
-            final int oldVersionCode = (int) settings.getLastBootedAppVersionCode();
-            final int versionCode = ContextUtil.getVersionCode(application);
-
-            FwLog.system("VersionCode [%d] -> [%d]", oldVersionCode, versionCode);
-            FwLog.system("VersionName [%s] -> [%s]", oldVersionName, versionName);
-
-            settings.setLastBootedAppVersionCode(versionCode);
-            settings.setLastBootedAppVersionName(versionName);
-
-            // バージョンコードかバージョン名が変わったら通知を行う
-            if (frameworkApplication != null && ((versionCode != oldVersionCode) || (!oldVersionName.equals(versionName)))) {
-                frameworkApplication.onApplicationUpdated(oldVersionCode, versionCode, oldVersionName, versionName);
-            }
-        }
-
-        // 設定をコミットする
-        settings.commit();
-
-        BehaviorSubject<LifecycleState> subject = BehaviorSubject.create(LifecycleState.NewObject);
-        gSubscriptionController.bind(subject);
-        subject.onNext(LifecycleState.OnResumed);
     }
 
     /**
@@ -76,14 +155,14 @@ public class FrameworkCentral {
      * MEMO : .start()は外部で呼び出す必要がある。
      */
     public static <T> RxTaskBuilder<T> newGlobalTask(RxTask.Async<T> callback) {
-        return new RxTaskBuilder<T>(gSubscriptionController)
+        return new RxTaskBuilder<T>(sImpl.mSubscriptionController)
                 .async(callback)
                 .observeOn(ObserveTarget.FireAndForget)
                 .subscribeOn(SubscribeTarget.Pipeline);
     }
 
     public static SubscriptionController getSubscription() {
-        return gSubscriptionController;
+        return sImpl.mSubscriptionController;
     }
 
     //    /**
@@ -119,11 +198,11 @@ public class FrameworkCentral {
      * Frameworkの設定クラスを取得する
      */
     public static BasicSettings getSettings() {
-        return settings;
+        return sImpl.mSettings;
     }
 
     public static Application getApplication() {
-        return application;
+        return sImpl.mApplication;
     }
 
     /**
@@ -162,5 +241,20 @@ public class FrameworkCentral {
          * @param newVersionName アップデート後のバージョン名
          */
         void onApplicationUpdated(int oldVersionCode, int newVersionCode, String oldVersionName, String newVersionName);
+
+        /**
+         * Class Unloadを防ぐため、Applicationのローカル変数としてCentralを保持する
+         */
+        void onRequestSaveCentral(Object central);
+
+        /**
+         * アプリが前面に配置された
+         */
+        void onApplicationForeground(Activity activity);
+
+        /**
+         * アプリがバックグラウンドに戻された
+         */
+        void onApplicationBackground();
     }
 }
