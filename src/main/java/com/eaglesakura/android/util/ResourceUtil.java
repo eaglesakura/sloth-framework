6b6ce4e66c508fb1f19376c720736459a218a970
eaglesakura <eagle.sakura@gmail.com>
2016-05-11 15:15:05 +0900

    単純なRecyclerViewを追加

--- src/main/java/com/eaglesakura/android/util/ResourceUtil.java
+++ src/main/java/com/eaglesakura/android/util/ResourceUtil.java
@@ -2,12 +2,24 @@ package com.eaglesakura.android.util;
 
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.ColorInt;
 import android.support.annotation.ColorRes;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
 import android.support.v4.content.res.ResourcesCompat;
 
 public class ResourceUtil {
+    @ColorInt
     public static int argb(@NonNull Context context, @ColorRes int colorId) {
         return ResourcesCompat.getColor(context.getResources(), colorId, context.getTheme());
+    }
+
+    @NonNull
+    public static Drawable drawable(@NonNull Context context, @DrawableRes int drawableId) {
+        if (drawableId == 0) {
+            return null;
+        }
+        return ResourcesCompat.getDrawable(context.getResources(), drawableId, context.getTheme());
     }
 }
