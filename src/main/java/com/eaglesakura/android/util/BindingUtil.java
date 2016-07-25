15f440438ac1c2710f78d9d98e0fdf813c8c95f1
eaglesakura <eagle.sakura@gmail.com>
2016-07-25 11:43:20 +0900

    公式と重複したUtilをdeprecatedに変更

--- src/main/java/com/eaglesakura/android/util/BindingUtil.java
+++ src/main/java/com/eaglesakura/android/util/BindingUtil.java
@@ -6,12 +6,14 @@ import android.databinding.ViewDataBinding;
 import android.view.View;
 
 public class BindingUtil {
+    @Deprecated
     public static <T extends ViewDataBinding> View bind(T binding) {
         // 自身をView自体に参照させる
         binding.getRoot().setTag(R.id.CardAdapter_DataBinding, binding);
         return binding.getRoot();
     }
 
+    @Deprecated
     public static <T extends ViewDataBinding> T from(Class<T> clazz, View view) {
         return (T) view.getTag(R.id.CardAdapter_DataBinding);
     }
