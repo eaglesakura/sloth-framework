dfa71ca79cb955af1afbec88d117101c2016b7f6
eaglesakura <eagle.sakura@gmail.com>
2016-10-07 14:23:39 +0900

    Notification生成Utilを追加

---
+++ src/main/java/com/eaglesakura/material/widget/support/SupportNotification.java
@@ -0,0 +1,40 @@
+package com.eaglesakura.material.widget.support;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+/**
+ * 通知バーの制御
+ */
+public class SupportNotification {
+
+    @NonNull
+    final Context mContext;
+
+    @NonNull
+    final Notification mNotification;
+
+    @NonNull
+    final NotificationManager mNotificationManager;
+
+    /**
+     * 通知ID
+     */
+    final int mNotificationId;
+
+    public SupportNotification(@NonNull Context context, @NonNull Notification notification, int notificationId) {
+        mContext = context;
+        mNotification = notification;
+        mNotificationId = notificationId;
+        mNotificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+    }
+
+    /**
+     * 通知をキャンセルする
+     */
+    public void cancel() {
+        mNotificationManager.cancel(mNotificationId);
+    }
+}
